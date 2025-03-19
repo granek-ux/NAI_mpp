7ea62023660011f4faf32ec43d04c3df8f0f4dd3
granek-ux <j.balinski@wp.pl>
2025-03-19 10:35:04 +0100

    wersja przed oddaniem projektu Java

--- mpp1/mpp1_JAVA/src/Pliki.java
+++ mpp1/mpp1_JAVA/src/Pliki.java
@@ -1,10 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Pliki {
@@ -29,10 +26,7 @@ public class Pliki {
                 String[] lines = line.split("\\s+");
                 List<Double> tmplist = new ArrayList<>();
                 for (int i = 0; i < lines.length - 1; i++) {
-                    if (!lines[i].isEmpty()) {
-
-                        tmplist.add(Double.parseDouble(lines[i].replaceAll(",", ".")));
-                    }
+                    if (!lines[i].isEmpty()) tmplist.add(Double.parseDouble(lines[i].replaceAll(",", ".")));
                 }
                 Obserwacja tmpobs = new Obserwacja(lines[lines.length - 1], tmplist);
                 dane.add(tmpobs);
@@ -45,11 +39,11 @@ public class Pliki {
 
     public void PorowanieDanych() {
         daneTreningowe = PobranieDanych(plikDanychTreningowych);
-        int nirgit =0;
-        for(Obserwacja obs : daneTestowe) {
+        int nirgit = 0;
+        for (Obserwacja obs : daneTestowe) {
             String name = DlaJednegoWektora(obs);
 
-            if(!name.equals(obs.getAtrybutDecyzyjny())) {
+            if (!name.equals(obs.getAtrybutDecyzyjny())) {
                 nirgit++;
             }
         }
@@ -57,33 +51,69 @@ public class Pliki {
 
         System.out.println("Procent  zgadzających się danych testowych z obliczeniami: " + procent + "%");
 
-        }
+    }
 
 
-    public String DlaJednegoWektora (Obserwacja obs) {
+    public String DlaJednegoWektora(Obserwacja obs) {
 
         List<NamedDistance> distances = new ArrayList<>();
         for (Obserwacja obs1 : daneTestowe) {
-            distances.add(CaculateDistance(obs1,obs));
+            distances.add(CaculateDistance(obs1, obs));
         }
 
-        Map<String, Integer> map = Calculations.getKNearest(distances, liczbaBliskichSasiadow);
+        Map<String, Integer> map = getKNearest(distances, liczbaBliskichSasiadow);
 
         return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
 
     }
 
-    public NamedDistance CaculateDistance(Obserwacja obserwacjaBazaDanych, Obserwacja obsnowy)
-    {
-        double distance = Calculations.Distance(obserwacjaBazaDanych.getListaAtrybutowwarunkowych(), obsnowy.getListaAtrybutowwarunkowych());
+    public NamedDistance CaculateDistance(Obserwacja obserwacjaBazaDanych, Obserwacja obsnowy) {
+        double distance = Distance(obserwacjaBazaDanych.getListaAtrybutowwarunkowych(), obsnowy.getListaAtrybutowwarunkowych());
 
         return new NamedDistance(obserwacjaBazaDanych.getAtrybutDecyzyjny(), distance);
     }
 
-    public int getVectorSize()
-    { return daneTreningowe.getFirst().getListaAtrybutowwarunkowych().size(); }
+    public int getVectorSize() {
+        return daneTreningowe.getFirst().getListaAtrybutowwarunkowych().size();
+    }
 
 
+    public static double Distance(List<Double> distances1, List<Double> distances2) throws IllegalArgumentException {
+        double distance = 0;
+        if (distances1.size() != distances2.size()) {
+            throw new IllegalArgumentException("Lists have different sizes");
+        } else {
+            for (int i = 0; i < distances1.size(); i++) {
+                distance += Math.pow(distances1.get(i) - distances2.get(i), 2);
+            }
+            distance = Math.sqrt(distance);
+        }
+        return distance;
+    }
 
+    public static Map<String, Integer> getKNearest(List<NamedDistance> distances, int numberOfK) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int k = 0; k < numberOfK; k++) {
+            double minDistance = Double.MAX_VALUE;
+            String minName = "";
+            int minPosition = 0;
+            for (int i = 0; i < distances.size(); i++) {
+                if (distances.get(i).getDistance() < minDistance) {
+                    minDistance = distances.get(i).getDistance();
+                    minName = distances.get(i).getName();
+                    minPosition = i;
+                }
+            }
+
+            distances.remove(minPosition);
+            if (map.containsKey(minName)) {
+                map.put(minName, map.get(minName) + 1);
+            } else {
+                map.put(minName, 1);
+            }
+
+        }
+        return map;
+    }
 
 }
